a41dd022131e8db11c9dfdc4252c4b8f17c1192a
GoogleOnStackOverflow <b01901111@ntu.edu.tw>
2015-11-15 05:08:06 +0800

    News Listener now working
    HOW TO: Use node data.js in par, you’ll have news paths and news object
    content in the directory.
    
    Known Issue:
    1. Even when receive no new news, the server still retrieve all the
    news again. (TODO: retrieve the new ones only)
    
    TODO:
    1. In the known issue
    2. Connect it to DB, that is, check the new paths and new news by data
    from DB, then upload the data which saved in local to DB

--- par/data.js
+++ par/data.js
@@ -1,6 +1,34 @@
+// import modules
 var http = require('http');
 var fs = require('fs');
+ 
 
+var newsHost = 'www.cna.com.tw';
+var lastPathRecieved = '';
+var newsPathsFile = './paths.txt';
+
+// helper function
+var getSubString = function(stringI, leadBy, endWith){
+	var startIndex = stringI.search(leadBy);
+	var endIndex = stringI.search(endWith);
+	if(startIndex === -1 || endIndex === -1) {
+		console.log('Can\'t find substring!');
+		return '';
+	}
+	var subStringToReturn = stringI.substring(startIndex + leadBy.length, endIndex);
+
+	return subStringToReturn;
+};
+
+/*
+	summary:
+			function that use http get request and get data
+
+	params: host (string): the host to send request
+			path (string): the path of the host to send request
+	return: httpGetRequest (promise)
+			use .then(sccessCallBack(data),errorCallBack(reason))
+*/
 var httpGetReturnRequestBody = function(host, path){
 	var httpTry = new Promise(function(resolve,reject){
 		try{
@@ -26,20 +54,25 @@ var httpGetReturnRequestBody = function(host, path){
 	return httpTry;
 };
 
+/*
+	summary:
+			http request response parser for CNA news site.
+
+	params: data (string) : data from http get request, which is a html file string
+	return: object{title,time,content}
+*/
 var htmlToNewsObject = function(data){
 	// Get Title
 	var titleLeader = '<h1 itemprop="headline">';
 	var titleTailer = '</h1>';
-	var titleStartIndex = data.search(titleLeader) + titleLeader.length;
-	var titleEndIndex = data.search(titleTailer);
-
-	var title = data.substring(titleStartIndex,titleEndIndex);
+	var title = getSubString(data,titleLeader,titleTailer);
 
 	//Get Time Info
 	var timeStartLeader = '<p>發稿時間：';
 	var timeStartIndex = data.search(timeStartLeader);
 	var postTime = data.substring(timeStartIndex + timeStartLeader.length, timeStartIndex + timeStartLeader.length +16);
 
+	// Check the last renew time
 	var renewTimeStartLeader = '最新更新：';
 	var renewTimeStartIndex = data.search(renewTimeStartLeader);
 	if(renewTimeStartIndex !== -1){
@@ -47,15 +80,23 @@ var htmlToNewsObject = function(data){
 	}
 
 	// Get News Content
-	var contextLeader = '</div><p>';
-	var contextTailer = '<br /><br />※你可能還想看：<br /><br />';
-	var contextStartIndex = data.search(contextLeader) + contextLeader.length;
+	var contextLeader = '<p>（中央社';
+	var contextTailer = /[0-9]{7}\s*(<\s*[\/]?p>|<br\s*[\/]?><br\s*[\/]?>※你可能還想看：)/;
+	var contextStartIndex = data.search(contextLeader);
 	var contextEndIndex = data.search(contextTailer);
+	if(contextStartIndex === -1) console.log('Cant find symbol: ' + contextLeader);
+	if(contextEndIndex === -1) console.log('Cant find symbol: tailer');
 
-	var context = data.substring(contextStartIndex,contextEndIndex);
+	if( contextEndIndex !== -1 && contextStartIndex !== -1)
+		var context = data.substring(contextStartIndex + 3, contextEndIndex);
+	else if(contextEndIndex === -1)
+		var context = 'New Content trans error! (end)';
+	else
+		var context = 'New Content trans error! (start)';
 	var brReplacer = /<br\s*[\/]?><br\s*[\/]?>/gi;
 	context = context.replace(brReplacer,'\n');
 	
+	// Construct the news object to return
 	var newsObjectToReturn = {
 		title : title,
 		time: postTime,
@@ -65,9 +106,114 @@ var htmlToNewsObject = function(data){
 	return newsObjectToReturn;
 }
 
+/*
+	summary:
+			http request response from CNA news site which contains paths.
 
-httpGetReturnRequestBody('www.cna.com.tw','/news/firstnews/201511045022.aspx').then(
+	return: Paths(String Array)
+*/
+var getAALLNewsLinks = function(){
+	var newsLinks = new Promise(function(resolve,reject){
+		console.log('Checking news from CNA...')
+		httpGetReturnRequestBody(newsHost,'/list/aall-'+ '1' +'.aspx').then(
+			function(data){
+				var articleLeader = '<div class="article_list">';
+				var articalTailer = 'class="pagination">';
+				var articlesSubString = getSubString(data,articleLeader,articalTailer);
+
+				//console.log(articlesSubString);
+				var index = 0;
+				var pathgetToken = articlesSubString.match(/(<a href=')(.*)('>)/g);
+			
+				for(var i = 0 ; i < pathgetToken.length ; i++){
+					pathgetToken[i] = pathgetToken[i].substring(9,pathgetToken[i].length-3) + 'x';
+				}
+
+				resolve(pathgetToken);
+			},
+			function(reason){
+				reject(reason);
+			}
+		);
+	});
+
+	return newsLinks;
+}
+
+// TODO: Should have a version to online DB
+var updatePathsToFileByArray = function(path,array){
+	var promiseToReturn = new Promise(function(recieve,reject){
+		try{
+			var exitPaths = fs.readFileSync(path).toString().split('\n');
+			var newPathIndex = (exitPaths.length >= 2)? array.indexOf(exitPaths[exitPaths.length - 2]) : array.length;
+			newPathIndex = (newPathIndex === -1)? array.length : newPathIndex;
+			if(newPathIndex === 0){
+				console.log('No news from CNA now.');
+			}else{
+				console.log('Got ' + newPathIndex + ' news from CNA, retrieving paths...');
+			}
+
+			for(var i = newPathIndex - 1 ; i >= 0 ; i--){
+				fs.appendFileSync(path, array[i].toString() + '\n');
+				console.log('Path: '+ array[i] + ' written to '+ path);
+			};
+			recieve(fs.closeSync(newPathIndex));
+		}catch(err){
+			reject(err);
+		}
+	});
+
+
+	return promiseToReturn;
+}
+
+var getAllNewsObjectByPathsArray = function(){
+	console.log('Retrieveing news content');
+
+	var array = fs.readFileSync(newsPathsFile).toString().split('\n');
+	if (array.length <=2) return;
+
+	fs.writeFile('./news.txt','');
+	array.forEach(function(path){
+		if(path.search('/news/') !== -1){
+			httpGetReturnRequestBody('www.cna.com.tw',path).then(
+				function(data){
+					var news = htmlToNewsObject(data);
+						news.path = path;
+						fs.appendFileSync('./news.txt', 'Title: ' + news.title.toString() + '\n');
+						fs.appendFileSync('./news.txt', 'Path: ' + news.path.toString() + '\n');
+						fs.appendFileSync('./news.txt', 'Time: ' + news.time.toString() + '\n');
+						fs.appendFileSync('./news.txt', news.content.toString() + '\n\n\n\n');
+				},
+				function(reason){
+					console.log(reason);
+				}
+			)
+		}
+	});
+}
+
+// main
+console.log('News Listener now on!');
+setInterval(function(){
+	getAALLNewsLinks().then(function(data){
+		updatePathsToFileByArray(newsPathsFile,data).then(function(){
+			getAllNewsObjectByPathsArray();
+		},function(err){
+			console.log(err);
+		});
+	},function(reason){
+		console.log(reason);
+	});
+},5*60*1000);
+
+
+
+/*
+httpGetReturnRequestBody('www.cna.com.tw','/news/firstnews/201511145024-1.aspx').then(
 	function(data){
+		fs.appendFileSync('./test.txt', data + '\n');
+		//console.log(data)
 		var news = htmlToNewsObject(data);
 		console.log(news.title);
 		console.log(news.time);
@@ -77,6 +223,4 @@ httpGetReturnRequestBody('www.cna.com.tw','/news/firstnews/201511045022.aspx').t
 		console.log(reason);
 	}
 )
-
-
-
+*/
